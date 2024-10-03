7d7103af71c7716fb56d1213a26b9bfca5bea27e
KimTaeEun1632 <qpwoal1324@naver.com>
2024-10-03 17:30:32 +0900

    프로젝트 리스트 컴포넌트

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner/Banner";
+import ProjectList from "./ProjectList/ProjectList";
 
 const Main = () => {
   return (
     <div>
       <Banner />
+      <ProjectList />
     </div>
   );
 };
